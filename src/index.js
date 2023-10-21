092f2beb160c94f83a50e321a988625ac6cbcb56
Renan Kozuka Micali <renanmicali@hotmail.com>
2023-10-21 16:05:36 +1100

    complete basics / come back for more in dept

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (event) => {
+      event.preventDefault()
+      addToDo(event.target.querySelector("#new-task-description").value)
+  })
+  
+  
 });
+
+
+function addToDo(todo){
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+  p.addEventListener('click', event => p.remove())
+}
